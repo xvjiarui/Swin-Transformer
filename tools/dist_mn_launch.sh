dac509dc833dc686b14389c3a26a7e0db66cc568
Jiarui XU <xvjiarui0826@gmail.com>
2021-06-17 15:55:07 -0700

    fixed multi-node launch

--- tools/dist_mn_launch.sh
+++ tools/dist_mn_launch.sh
@@ -5,7 +5,7 @@ NODES=$2
 GPUS_PER_NODE=$3
 PORT=${PORT:-29500}
 
-PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH OMP_NUM_THREADS=4 \
 python -m torch.distributed.launch --nproc_per_node=$GPUS_PER_NODE \
   --nnodes=$NGC_ARRAY_SIZE --node_rank=$NGC_ARRAY_INDEX --master_port=$PORT \
-    main.py --cfg $CONFIG ${@:3}
+    main.py --cfg $CONFIG ${@:4}
