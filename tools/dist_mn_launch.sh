e34f154476fd8232795f11f5f6a904cf339bfffe
Jiarui XU <xvjiarui0826@gmail.com>
2021-06-17 19:53:04 -0700

    add master addr

--- tools/dist_mn_launch.sh
+++ tools/dist_mn_launch.sh
@@ -7,5 +7,6 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH OMP_NUM_THREADS=4 \
 python -m torch.distributed.launch --nproc_per_node=$GPUS_PER_NODE \
-  --nnodes=$NGC_ARRAY_SIZE --node_rank=$NGC_ARRAY_INDEX --master_port=$PORT \
+  --nnodes=${NGC_ARRAY_SIZE} --node_rank=${NGC_ARRAY_INDEX} \
+  --master_addr=${NGC_MASTER_ADDR}  \
     main.py --cfg $CONFIG ${@:4}
