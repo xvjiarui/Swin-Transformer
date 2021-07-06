b66e45952d8d96603c406baf3a41cb8d26bb3dfc
Jiarui XU <xvjiarui0826@gmail.com>
2021-07-06 15:20:27 -0700

    set OMP_NUM_THREADS=10

--- tools/dist_mn_launch.sh
+++ tools/dist_mn_launch.sh
@@ -5,7 +5,7 @@ NODES=$2
 GPUS_PER_NODE=$3
 PORT=${PORT:-29500}
 
-PYTHONPATH="$(dirname $0)/..":$PYTHONPATH OMP_NUM_THREADS=4 \
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH OMP_NUM_THREADS=10 \
 python -m torch.distributed.launch --nproc_per_node=$GPUS_PER_NODE \
   --nnodes=${NGC_ARRAY_SIZE} --node_rank=${NGC_ARRAY_INDEX} \
   --master_addr=${NGC_MASTER_ADDR}  \
